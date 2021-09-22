9893ec5e47b2d107bda1bcc0a3b28fba5cd3cebb
mehdi khaddor <mehdi@khaddor.Home>
2021-09-22 01:04:59 +0100

    admin LTE ++ created models

---
+++ database/migrations/2021_09_21_235950_create_properties_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('city');
+            $table->text('address');
+            $table->string('type');
+            $table->text('description');
+            $table->integer('isRented')->default('0');
+            $table->timestamp('rent_date');
+            $table->timestamp('rend_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+
+@section('content_header')
+    <h1>Dashboard</h1><hr>
+@stop
+
+@section('content')
+    <div class="container bg-secondary p-2">
+        <h3>Title</h3>
+        <p>Welcome to this beautiful admin panel.</p>
+    </div>
+
+@stop
+
